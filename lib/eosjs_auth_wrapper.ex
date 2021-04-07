88b1c668810b335b3d66407753285fbfa3c46a22
venomnert <neerthigan@gmail.com>
2021-04-06 23:12:34 -0400

    Removed comment

--- lib/eosjs_auth_wrapper.ex
+++ lib/eosjs_auth_wrapper.ex
@@ -1,17 +1,4 @@
 defmodule EosjsAuthWrapper do
-  @moduledoc """
-  Documentation for `EosjsAuthWrapper`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EosjsAuthWrapper.hello()
-      :world
-
-  """
   def hello do
     :world
   end

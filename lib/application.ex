472897a674f422b6edaf00dd4b6d35fb56cc6469
venomnert <neerthigan@gmail.com>
2021-04-08 22:42:31 -0400

    added node_auth inside of lib

--- lib/application.ex
+++ lib/application.ex
@@ -6,10 +6,19 @@ defmodule EosjsAuthWrapper.Application do
   require Logger
 
   def start(_type, _args) do
+    node_path = Path.join([File.cwd!(), "lib/nodejs_auth/js/"]) |> IO.inspect(label: "PATH")
+
     children = [
       %{
         id: NodeJS,
-        start: {NodeJS, :start_link, [[path: "./nodejs_auth/js/", pool_size: 4]]}
+        start:
+          {NodeJS, :start_link,
+           [
+             [
+               path: node_path,
+               pool_size: 4
+             ]
+           ]}
       }
     ]
 
